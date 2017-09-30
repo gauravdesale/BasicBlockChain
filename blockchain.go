2d11c92945c871262da4b5f39625e2b1dcaa95c1
gauravdesale <desalegaurav60@gmail.com>
2017-09-30 00:27:00 -0500

    added new blovk generators but now need to add the authentication section

--- blockchain.go
+++ blockchain.go
@@ -11,10 +11,19 @@ func constructor(index, timeStamp, data, previousHash = ''){
 	this.data = data
 	this.previousHash = previousHash
 	this.hash = this.calculateHash()
+	return newblock;
 }
 func New384() hash.Hash { return &state{index, timeStamp, previousHash}.toString() }
 
 func  createNewBlock(){
 	return New384(0, "09/28/17", "genesisblock", "0")
 }
+func getLatestBlock(){
+	//still need to figure this one out
+func addBlock(){
+	previousHash = getLatestBLock().hash
+	newBlock = calculateHash()
+	constructor().push()
+}
+
 
